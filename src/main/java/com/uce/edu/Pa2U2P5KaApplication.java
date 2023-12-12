9966f408bf2277076d4d11ad9cc69860c40bfcca
kevoaac <kevinandradecardenas@gmail.com>
2023-12-11 19:44:10 -0500

    Primer Commit - aplication.properties

---
+++ src/main/java/com/uce/edu/Pa2U2P5KaApplication.java
@@ -0,0 +1,18 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P5KaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P5KaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\nHOLA MUNDO!\n");
+	}
+}
